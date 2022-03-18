7550548e3c0ac9d44ba546ec2f26a04fd7442e1b
Ivan B <van.mohntenheim@gmail.com>
2022-03-19 04:17:28 +0700

    fix vscode autofix

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: ['plugin:vue/vue3-recommended'],
+};
